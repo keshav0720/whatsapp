c84f506d3c9e75229eadbcc78babd9726a0984af
Gaurav Chadee <hiji762@yahoo.com>
2023-04-23 13:40:00 +0400

    error correction on login logic

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,26 @@
 import Head from 'next/head';
-// import Image from 'next/image'
-// import { Inter } from '@next/font/google'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css';
 import { auth, db } from '../firebase';
 import * as EmailValidator from "email-validator";
 import { useAuthState } from 'react-firebase-hooks/auth';
+import {useCollection} from 'react-firebase-hooks/firestore';
 
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 	const [user] = useAuthState(auth);
+	const userChatsRef = db.collection('chats').where("users", 'array-contains', user.email);
+	const [chatsSnapshot, loading, error] = useCollection(userChatsRef);
+
 	const createChat = () => {
 		const input = prompt('Please enter an email address for the user');
 
 		if (!input) return;
 
 		if (EmailValidator.validate(input)) {
-			db.collection('chats').add({
+			db.collection("chats").add({
 				users: [user.email, input],
 			});
 
@@ -31,6 +35,14 @@ export default function Home() {
       </Head>
 			<center>
 				<h1>Your chats</h1>
+			</center>
+				{chatsSnapshot?.docs.map((chat) => (
+					<div>
+						<li>{chat.data().users}</li>
+					</div>
+				))}
+
+			<center>
 				<button onClick={createChat}>Start a chat</button>
 				<button onClick={() => auth.signOut()}>Log out</button>
 			</center>
